c8b500935d865d13736666b819b0bdce680a1a44
KawaiiChino <552644808@qq.com>
2022-10-08 23:45:48 +0800

    tag📌:  开始第一步

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login_container">
+        登录组件
+    </div>
+
+</template>
+<script>
+export default{}
+
+</script>
+
+<style lang="less" scoped>
+    .login_container{
+        background-color: aqua;
+        height: 100%;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// 导入全局样式
+import './assets/css/global.css'
 import './plugins/element.js'
+import router from './router'
 
 Vue.config.productionTip = false
 
