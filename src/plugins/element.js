3939c16ae40fac166e178e3bd9968db6374cfcc1
KawaiiChino <552644808@qq.com>
2022-10-10 22:09:06 +0800

    feat✨:  day2

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,6 @@
-import Vue from 'vue'
-import { Button } from 'element-ui'
-
+import { Button, Form, FormItem, Input } from 'element-ui';
+import Vue from 'vue';
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
