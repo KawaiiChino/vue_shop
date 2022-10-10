3cb483e0b88d241466152002f7ac307e5d96fd90
KawaiiChino <552644808@qq.com>
2022-10-10 22:08:07 +0800

    feat✨:  day1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
 // 导入全局样式
@@ -5,6 +6,8 @@ import './assets/css/global.css'
 import './plugins/element.js'
 import router from './router'
 
+axios.defaults
+Vue.prototype.$http=axios
 Vue.config.productionTip = false
 
 new Vue({
